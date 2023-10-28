f0512e2cd05c674ed3a0e190522580431d6fce58
masonLenovoLaptop <masonscott2112@gmail.com>
2023-10-28 10:38:42 -0600

    reorganized things into functions so I don't get an annurism digging through all this OpenGL code

--- sceneview.cpp
+++ sceneview.cpp
@@ -7,16 +7,19 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+
 SceneView::SceneView(QWidget* parent): QOpenGLWidget(parent)
 {
     setMinimumSize(775, 775);
     setMaximumSize(775, 775);
 }
 
+
 SceneView::~SceneView()
 {
   teardownGL();
 }
+
 
 std::string SceneView::read_shader_code_from_resource_file(QString filepath)
 {
@@ -35,100 +38,123 @@ std::string SceneView::read_shader_code_from_resource_file(QString filepath)
   return data.toStdString();
 }
 
+
 QImage SceneView::read_texture_from_resource_file(QString filepath)
 {
   QImage texture(filepath);
   return texture;
 }
 
-void SceneView::initializeGL()
-{
-  initializeOpenGLFunctions();
-  printContextInformation();
 
-  vertexShaderCode = read_shader_code_from_resource_file(":/shaders/pass_through.vert");
-  fragmentShaderCode = read_shader_code_from_resource_file(":/shaders/simple.frag");
+//---------------------VERTEX SHADER INIT------------------------------
+void SceneView::initVertexShader(){
 
-  //Texture image reading from file using Qt
-  //textureImage = read_texture_from_resource_file(":/textures/container.png");
-  //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, textureImage.width(), textureImage.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE,
-  //textureImage.bits());
+    vertexShaderCode = read_shader_code_from_resource_file(":/shaders/pass_through.vert");
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const char* vertexShaderString = vertexShaderCode.c_str();
+    glShaderSource(vertexShader, 1, &vertexShaderString, NULL);
+    glCompileShader(vertexShader);
 
-  // build and compile our shader program
-  // ------------------------------------
-  // vertex shader
-  unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  const char* vertexShaderString = vertexShaderCode.c_str();
-  glShaderSource(vertexShader, 1, &vertexShaderString, NULL);
-  glCompileShader(vertexShader);
-  // check for shader compile errors
-  int success;
-  char infoLog[512];
-  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    checkVertShaderErrors();
+}
 
-  if (!success)
-  {
+void SceneView::checkVertShaderErrors(){
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
     glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
     qDebug() << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog;
-  }
+    }
+}
 
-  // fragment shader
-  unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  const char* fragmentShaderString = fragmentShaderCode.c_str();
-  glShaderSource(fragmentShader, 1, &fragmentShaderString, NULL);
-  glCompileShader(fragmentShader);
-  // check for shader compile errors
-  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 
-  if (!success)
-  {
+//---------------------FRAGMENT SHADER INIT---------------------------------
+void SceneView::initFragmentShader(){
+
+    fragmentShaderCode = read_shader_code_from_resource_file(":/shaders/simple.frag");
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* fragmentShaderString = fragmentShaderCode.c_str();
+    glShaderSource(fragmentShader, 1, &fragmentShaderString, NULL);
+    glCompileShader(fragmentShader);
+
+    checkFragShaderErrors();
+}
+
+
+void SceneView::checkFragShaderErrors(){
+    int success;
+    char infoLog[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
     glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
     qDebug() << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog;
-  }
+    }
+}
 
-  // link shaders
-  shaderProgram = glCreateProgram();
-  glAttachShader(shaderProgram, vertexShader);
-  glAttachShader(shaderProgram, fragmentShader);
-  glLinkProgram(shaderProgram);
-  // check for linking errors
-  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+void SceneView::linkShaders(){
 
-  if (!success)
-  {
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    checkLinkIssues();
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+}
+
+
+void SceneView::checkLinkIssues(){
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
     glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
     qDebug() << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog;
-  }
+    }
+}
 
-  glDeleteShader(vertexShader);
-  glDeleteShader(fragmentShader);
+void SceneView::initializeGL()
+{
+    initializeOpenGLFunctions();
+    printContextInformation();
 
-  // set up vertex data (and buffer(s)) and configure vertex attributes
-  // ------------------------------------------------------------------
-  float vertices[] =
-  {
+    initVertexShader();
+    initFragmentShader();
+    linkShaders();
+
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ----------------------------------------------------------------------------------------------------
+    float vertices[] =
+    {
     -0.5f, -0.5f, 0.0f, // left
     0.5f, -0.5f, 0.0f, // right
     0.0f, 0.5f, 0.0f // top
-  };
+    };
 
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-  glBindVertexArray(VAO);
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
 
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-  glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
-  // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-  // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-  glBindVertexArray(0);
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
 
 }
 
@@ -142,7 +168,7 @@ void SceneView::paintGL()
   trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
   trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
 
-  // draw our first triangle
+
   glUseProgram(shaderProgram);
 
   // link the uniform variable to the vertex shader
--- sceneview.hpp
+++ sceneview.hpp
@@ -7,29 +7,40 @@
 
 class SceneView : public QOpenGLWidget, protected QOpenGLExtraFunctions
 {
-  Q_OBJECT
+    Q_OBJECT
 public:
-  SceneView(QWidget* parent=nullptr);
-  ~SceneView();
+    SceneView(QWidget* parent=nullptr);
+    ~SceneView();
 
 protected:
-  void initializeGL() override;
-  void paintGL() override;
-  void resizeGL(int width, int height) override;
-  void teardownGL();
+    void initializeGL() override;
+    void paintGL() override;
+    void resizeGL(int width, int height) override;
+    void teardownGL();
 
 private:
-  std::string read_shader_code_from_resource_file(QString filepath);
-  QImage read_texture_from_resource_file(QString filepath);
-  void printContextInformation();
 
-  std::string vertexShaderCode;
-  std::string fragmentShaderCode;
-  QImage textureImage;
+    void initVertexShader();
+    void checkVertShaderErrors();
+    void initFragmentShader();
+    void checkFragShaderErrors();
+    void linkShaders();
+    void checkLinkIssues();
 
-  unsigned int shaderProgram{0};
-  unsigned int VBO{0};
-  unsigned int VAO{0};
+    std::string read_shader_code_from_resource_file(QString filepath);
+    std::string vertexShaderCode;
+    std::string fragmentShaderCode;
+
+    unsigned int vertexShader{0};
+    unsigned int fragmentShader{0};
+    unsigned int shaderProgram{0};
+    unsigned int VBO{0};
+    unsigned int VAO{0};
+    unsigned int EBO{0};
+
+    QImage read_texture_from_resource_file(QString filepath);
+    void printContextInformation();
+
 };
 
 #endif // SCENEVIEW_HPP
